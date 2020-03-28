44e6488e6ec86bbdc4ba6df46ad4b0b0f6a46e60
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2020-03-28 13:48:06 -0300

    Add ios startup files.

---
+++ App/Source/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Hanashi
+//
+//  Created by Anderson Santos Gusmão on 23/03/20.
+//  Copyright © 2020 Anderson Santos Gusmão. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let _ = (scene as? UIWindowScene) else { return }
+	}
+}
